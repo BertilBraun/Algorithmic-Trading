61eb5668b406b4f50cc675189654144569d53104
Bertil <bertil.braun.private@gmail.com>
2022-02-18 00:09:24 +0100

    cleanup

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = "https://paper-api.alpaca.markets"
 ALPACA_API_KEY = os.getenv('ALPACA_API_KEY')
 ALPACA_SECRET_KEY = os.getenv('ALPACA_SECRET_KEY')
+PAPER = os.getenv('PAPER') != 'False'
 
-api = tradeapi.REST(key_id=ALPACA_API_KEY, secret_key=ALPACA_SECRET_KEY,
-                    base_url=BASE_URL, api_version='v2')
+api = tradeapi.REST(
+    key_id=ALPACA_API_KEY,
+    secret_key=ALPACA_SECRET_KEY,
+)
